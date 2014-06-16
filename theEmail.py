b4ed7ff1ee8ade1793aa9a3dc14305838dd8e1ca
micro100 <jwoods@micro100.com>
2014-06-16 15:56:45 -0600

    Update theEmail.py

--- theEmail.py
+++ theEmail.py
@@ -4,7 +4,7 @@ from email.mime.multipart import MIMEMultipart
 
 address_book = ['xxxx@xxx.com', 'xxxx@xxx.com','xxxx@xxx.com']
 msg = MIMEMultipart()    
-sender = 'Alert@micro100.com'
+sender = 'xxxx@xxx.com'
 subject = "Pi 80 Alarm"
 body = "Pi 80 Alarm issue!!!"
 
@@ -15,6 +15,6 @@ msg.attach(MIMEText(body, 'plain'))
 text=msg.as_string()
 #print text
 # Send the message via our SMTP server
-s = smtplib.SMTP('10.103.1.17:25')
+s = smtplib.SMTP('xxxxxxxxxx')
 s.sendmail(sender,address_book, text)
 s.quit() 
